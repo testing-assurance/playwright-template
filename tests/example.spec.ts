f8abf53d68dd8847baecc895fa761a0616e69666
Sajad Abbasi <s.abbasi@me.com>
2024-12-08 18:38:59 +0330

    refactor: unify string quotes in example.spec.ts for consistency

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,14 +1,13 @@
-import { expect } from "@playwright/test";
-import { test } from "../core/base-test";
+import { expect } from '@playwright/test';
+import { test } from '../core/base-test';
 
-test.describe("Sample", () => {
-  test("TA-001: Failed Test 2", async ({ page }) => {
-    await page.goto("https://www.google.com");
-    expect(page.url()).toBe("https://www.failed.com");
-    expect(2).toBe(1);
+test.describe('Sample', () => {
+  test('TA-001: Failed Test 2', async ({ page }) => {
+    await page.goto('https://www.google.com');
+    expect(page.url()).toBe('https://www.failed.com');
   });
 
-  test("TA-002: Passed Test", async () => {
-    expect(1).toBe(2);
+  test('TA-002: Passed Test', async () => {
+    expect(1).toBe(1);
   });
 });
