a72fc65c739e5f9a00a297525d4b5bf3f4fff7a3
Sajad Abbasi <s.abbasi@me.com>
2024-12-08 18:41:04 +0330

    fix: update test name for clarity in example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,7 +2,7 @@ import { expect } from '@playwright/test';
 import { test } from '../core/base-test';
 
 test.describe('Sample', () => {
-  test('TA-001: Failed Test 2', async ({ page }) => {
+  test('TA-001: Failed Test', async ({ page }) => {
     await page.goto('https://www.google.com');
     expect(page.url()).toBe('https://www.failed.com');
   });
