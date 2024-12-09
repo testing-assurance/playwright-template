0113a8b4c87af647a6211798f14f007268b00553
Sajad Abbasi <s.abbasi@me.com>
2024-12-09 13:10:27 +0330

    chore: comment out test cases in example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,13 +1,13 @@
-import { expect } from '@playwright/test';
-import { test } from '../core/base-test';
+// import { expect } from '@playwright/test';
+// import { test } from '../core/base-test';
 
-test.describe('Sample', () => {
-  test.skip('TA-001: Failed Test', async ({ page }) => {
-    await page.goto('https://www.google.com');
-    expect(page.url()).toBe('https://www.failed.com');
-  });
+// test.describe('Sample', () => {
+//   test.skip('TA-001: Failed Test', async ({ page }) => {
+//     await page.goto('https://www.google.com');
+//     expect(page.url()).toBe('https://www.failed.com');
+//   });
 
-  test.skip('TA-002: Passed Test', async () => {
-    expect(1).toBe(1);
-  });
-});
+//   test.skip('TA-002: Passed Test', async () => {
+//     expect(1).toBe(1);
+//   });
+// });
