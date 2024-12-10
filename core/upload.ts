99cc6da61422e8be431405f0ae5f0e500a5b090c
Sajad Abbasi <s.abbasi@me.com>
2024-12-10 17:16:04 +0330

    ci: add HandBrake.js for video format conversion and update package.json

--- core/upload.ts
+++ core/upload.ts
@@ -3,6 +3,7 @@ import { FailedTests, Spec } from './types';
 import { getIdFromTitle } from './utils';
 import { put } from '@vercel/blob';
 import fs from 'fs';
+import hbjs from 'handbrake-js';
 
 const projectId = process.argv[2];
 
@@ -33,14 +34,29 @@ function getList(suites: Suites): FailedTests[] {
   return failedTests;
 }
 
+function convertToM4V(videoLocation: string): { outputUrl: string } {
+  const output = videoLocation.replace('.webm', '.m4v');
+  hbjs
+    .spawn({ input: videoLocation, output: output })
+    .on('error', (err) => {
+      // invalid user input, no video found etc
+      console.log('Error converting video:', err);
+    })
+    .on('end', () => {
+      console.log('Video conversion completed...');
+    });
+  return { outputUrl: output };
+}
+
 const list = getList(results.suites);
 
 function uploadVideos(list: ReturnType<typeof getList>) {
-  list.forEach((item) => {
+  list.forEach(async (item) => {
     if (item.videoLocation) {
       const videoBuffer = fs.readFileSync(item.videoLocation);
+      const { outputUrl } = convertToM4V(item.videoLocation);
       // do it one by one instead of doing it in the loop to prevent potential upload errors
-      put(`bug-videos/${projectId}/${item.testId}/${item.environment}/video.webm`, videoBuffer, {
+      put(outputUrl, videoBuffer, {
         access: 'public',
         token: 'vercel_blob_rw_Xp8sfOuFASN7ZePB_dmu8W8LS3LJ8mIXh9fYeaVdHYuxMHE',
         addRandomSuffix: false,
