9a7513f346ae0c187a81b9937a6abc54cd210ea4
Sajad Abbasi <s.abbasi@me.com>
2024-12-13 18:15:00 +0330

    feat: add logs to sync-board

--- core/sync-board.ts
+++ core/sync-board.ts
@@ -2,7 +2,10 @@ const projectId = process.argv[2];
 const baseURL = 'https://ta-app-git-preview-sajad-abbasis-projects.vercel.app';
 
 function syncBoard(projectId: string) {
+  console.log('Syncing board for projectId: ', projectId);
   return fetch(`${baseURL}/api/board-integrations/notion/sync?projectId=${projectId}`);
 }
 
-syncBoard(projectId).then(() => console.log('Syncing board...'));
+syncBoard(projectId)
+  .then(() => console.log('Syncing board...'))
+  .catch((err) => console.error('Error syncing board:', err));
